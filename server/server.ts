fbb3644f7c4da930a2ad79948e3190a1f11242f4
Adam Hall <adamhalldesigns@gmail.com>
2021-11-29 11:20:52 +0000

    basic express router

--- server/server.ts
+++ server/server.ts
@@ -1,25 +1,14 @@
-import { createServer, Server } from 'http';
+import express from 'express';
 import { Hello } from '../shared/types';
 
-let serverInstance: Server;
+const app = express();
 const port = 4000;
 
+app.get('/api', (_, out) => {
+  const payload: Hello = { ok: true };
+  out.json(payload);
+});
+
 export default async function server(): Promise<number> {
-  serverInstance = createServer((req, res) => {
-    console.log('req -->', req.url);
-
-    const payload: Hello = { ok: true };
-
-    res.writeHead(200, {
-      'Content-Type': 'application/json',
-    });
-    res.write(JSON.stringify(payload));
-    res.end();
-  });
-
-  return new Promise((resolve) => {
-    serverInstance.listen(port, () => {
-      resolve(port);
-    });
-  });
+  return new Promise((res) => app.listen(port, () => res(port)));
 }
